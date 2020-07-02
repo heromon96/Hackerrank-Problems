85a89146cfe6d46b31a981fd46abfa15c70d1ff4
Harsh Sharma <Harsh Sharma@DESKTOP-N6R0FO5>
2020-07-02 11:09:22 +0530

    Repeated string need larger computation test cases how do I resolve it.

--- Hackerrank/src/repeated_string/Solution.java
+++ Hackerrank/src/repeated_string/Solution.java
@@ -42,6 +42,6 @@ public class Solution {
     
     public static void main(String[] args)
     {
-        System.out.println(Solution.repeatedString("aba", 10));
+        System.out.println(Solution.repeatedString("a", 1000000000000L));
     }
 }
